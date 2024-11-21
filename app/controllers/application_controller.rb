ef4687ace35c372ce45c431eb1502a86508ebe45
Misheck12 <Misheck1720@gmail.com>
2024-11-21 17:33:25 +0000

    Nov 21, 2024, 7:33 PM

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, except: [:home]
+  before_action :authenticate_user!, except: [:home, :home] 
+
+  # ... rest of your ApplicationController code ...
 end
