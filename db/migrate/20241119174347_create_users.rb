10dd4821e5edae87f5d06d63d0ef168a109e72a6
Misheck12 <Misheck1720@gmail.com>
2024-11-21 21:38:31 +0000

    Nov 21, 2024, 11:38 PM

--- db/migrate/20241119174347_create_users.rb
+++ db/migrate/20241119174347_create_users.rb
@@ -6,9 +6,16 @@ class CreateUsers < ActiveRecord::Migration[7.0]
       t.string "encrypted_password", default: "", null: false
       t.string "reset_password_token"
       t.datetime "reset_password_sent_at"
-      t.datetime "remember_created_at"      t.string :role
-
+      t.datetime "remember_created_at"
+      t.string :role
+      
       t.timestamps
     end
+
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
+    # If using confirmable or lockable modules, uncomment the following lines:
+    # add_index :users, :confirmation_token,   unique: true
+    # add_index :users, :unlock_token,         unique: true
   end
 end
