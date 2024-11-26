159cb393f60dec5bace84b8c553b1fdf5a463600
Misheck12 <Misheck1720@gmail.com>
2024-11-26 09:34:41 +0000

    Nov 26, 2024, 11:34 AM

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,22 @@
 class SessionsController < ApplicationController
+  # Include Devise's helpers
+  include Devise::Controllers::Helpers
+
+  # Skip authentication for login and registration actions
   skip_before_action :authenticate_user!, only: [:new, :create]
 
   def new
-    # Render the login form
+    # Renders the login form (sessions/new.html.erb)
   end
 
   def create
     user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect_to rootdashboard_path, notice: 'Logged in successfully.'
+
+    if user&.valid_password?(params[:password])
+      sign_in(user)
+      
+      # Redirect based on user role
+      redirect_to after_sign_in_path_for(user), notice: 'Logged in successfully.'
     else
       flash.now[:alert] = 'Invalid email or password'
       render :new
@@ -17,7 +24,22 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session[:user_id] = nil
+    sign_out(current_user)
     redirect_to root_path, notice: 'Logged out successfully.'
+  end
+
+  protected
+
+  # Customize the redirect path after sign in
+  def after_sign_in_path_for(resource)
+    if resource.admin?
+      admin_dashboard_path
+    elsif resource.team?
+      team_dashboard_path
+    elsif resource.referee?
+      referee_dashboard_path
+    else
+      dashboard_path
+    end
   end
 end
