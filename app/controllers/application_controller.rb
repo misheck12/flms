83df24184b5ff7b9368974f1d5e9ceee3c327c27
Misheck12 <Misheck1720@gmail.com>
2024-11-26 10:52:33 +0000

    Nov 26, 2024, 12:52 PM

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, except: [:home, :home] 
+  before_action :authenticate_user!, except: [:home] 
 
-  # ... rest of your ApplicationController code ...
+  def after_sign_in_path_for(resource)
+    # Redirect based on user role
+    case resource.role
+    when 'admin'
+      admin_dashboard_path
+    when 'team'
+      team_dashboard_path
+    when 'referee'
+      referee_dashboard_path
+    else
+      dashboard_path # Default dashboard for other roles
+    end
+  end
 end
