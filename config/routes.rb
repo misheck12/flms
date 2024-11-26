68f400ce219f0776c2d15fbd4ef352ae39265c6a
Misheck12 <Misheck1720@gmail.com>
2024-11-26 11:04:50 +0000

    Nov 26, 2024, 1:04 PM

--- config/routes.rb
+++ config/routes.rb
@@ -72,11 +72,4 @@ Rails.application.routes.draw do
 
     resources :teams, only: [:index]
   end
-
-  # Error Pages
-  match "/404", to: "errors#not_found", via: :all
-  match "/500", to: "errors#internal_server_error", via: :all
-
-  # Catch-All Route for Undefined Paths (Optional)
-  match "*path", to: "errors#not_found", via: :all
 end
