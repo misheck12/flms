a11d152a5fba7fad6cdf6e36e9728b397ca6c1d5
Misheck12 <Misheck1720@gmail.com>
2024-11-26 10:19:30 +0000

    Nov 26, 2024, 12:19 PM

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,36 +4,37 @@ class RegistrationsController < Devise::RegistrationsController
   # Before actions to permit additional parameters
   before_action :configure_permitted_parameters, only: [:create, :update]
 
-  protected
-
-  # Permit additional parameters (name and role)
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :role])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :role])
-  end
-
-  # Override the create method to prevent auto sign-in
+  # Ensure the create action is public by placing it above 'protected'
   def create
     build_resource(sign_up_params)
 
     resource.save
     yield resource if block_given?
+
     if resource.persisted?
       if resource.active_for_authentication?
-        # Do NOT sign in the user
+        # Do NOT sign in the user automatically
         set_flash_message! :notice, :signed_up
         # Redirect to the login page
-        respond_with resource, location: new_user_session_path
+        redirect_to new_user_session_path
       else
         expire_data_after_sign_in!
-        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+        redirect_to after_inactive_sign_up_path_for(resource)
       end
     else
-      # If user failed to save, render the registration form again
+      # Render the registration form again with errors
       clean_up_passwords resource
       set_minimum_password_length
       respond_with resource
     end
+  end
+
+  protected
+
+  # Permit additional parameters (name and role)
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:role])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:role])
   end
 
   # Redirect path after sign up (login page)
