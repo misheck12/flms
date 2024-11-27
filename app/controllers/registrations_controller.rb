30d0ecf50329f27878d20312b4c7af3bedde803f
Misheck12 <Misheck1720@gmail.com>
2024-11-27 11:02:09 +0000

    Nov 27, 2024, 1:02 PM

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -13,4 +13,17 @@ class RegistrationsController < Devise::RegistrationsController
     # Permit the :role attribute for account update (optional)
     devise_parameter_sanitizer.permit(:account_update, keys: [:role])
   end
+
+  def after_sign_in_path_for(resource)
+    case resource.role
+    when 'team'
+      team_dashboard_path  # Redirect team users to their dashboard
+    when 'admin'
+      admin_dashboard_path  # Redirect admin users to their dashboard
+    when 'referee'
+      referee_dashboard_path  # Redirect referee users to their dashboard
+    else
+      super # Use the default Devise redirect if the role is not recognized
+    end
+  end
 end
